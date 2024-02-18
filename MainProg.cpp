262c96dad2fc0b4d8fdbf5a7e4509ae894c3ceac
khagendra01 <khagendrakhatri365@gmail.com>
2024-02-17 23:39:16 -0500

    main program

---
+++ MainProg.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+#include "strlib.hpp"
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello" ;
+    cout << "Hello" ;
+    cout << "Hello" ;
+	cout << "Hello" ;
+    cout << endl;
+
+    cst:: String myString;
+    myString = "This is jesus love";
+    cout << myString ;
+    return 0;
+}
+
+
